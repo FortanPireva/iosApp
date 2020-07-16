898920775b5ca7466e473a25a06e5b2f9e12d7d0
Fortan <fortan@Fortans-iMac.local>
2020-07-17 01:23:42 +0200

    Added a tableView with a custom tableviewcontroller

---
+++ iosApp/TableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TableViewController.swift
+//  iosApp
+//
+//  Created by Fortan on 7/17/20.
+//  Copyright © 2020 GrFL. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names=[
+        "john doe",
+        "john doe2",
+        "john doe3"
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate=self
+        tableView.dataSource=self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension TableViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me")
+    }
+}
+
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "cell",for:indexPath)
+        cell.textLabel?.text=names[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
