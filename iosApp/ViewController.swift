7a05eb47c3cabb1d9956ba1f1a37b628192f93f3
Fortan <fortan@Fortans-iMac.local>
2020-07-16 23:52:47 +0200

    First Commit

--- iosApp/ViewController.swift
+++ iosApp/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func button(_ sender: UIButton) {
+        label.text=textfield.text
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
